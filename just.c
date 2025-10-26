4870099f6181e9bcbcd4c863bc498dcee725ab60
dipan313 <164169398+dipan313@users.noreply.github.com>
2025-10-26 20:10:14 +0530

    add new file

---
+++ just.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char a[50], b[50];
+    printf("Enter a string");
+    scanf("%s", a);
+    printf("Enter a string");
+    scanf("%s", b);
+    printf("the output is %s, %s", a, b);
+    return 0;
+}
